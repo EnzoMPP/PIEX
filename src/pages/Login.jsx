2fd1a857e1e330195b6c33e0ea347f23dc2affd5
Gabriel Martins <gabriel@martins.com>
2024-11-10 18:39:14 -0300

    Tela de Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import "../pages/Login.css";
 
 export const Login = () => {
 
 
-    return(
-
-        <div>
-            Login
+    return (
+        <div className="container">
+            <h2 className="title">Entrar</h2>
+            <img src="./src/assets/lugo.jpg" alt="Logo" className="logo" />
+            <input
+                type="email"
+                placeholder="E-mail"
+                className="input"
+            />
+            <input
+                type="password"
+                placeholder="Senha"
+                className="input"
+            />
+            <a href="#" className="senha">Redefinir Senha</a>
+            <button className="button">
+                Login
+            </button>
         </div>
     )
 }
